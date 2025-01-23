c48bab68d51cb013faa45ede7cb7252074341391
Čestmír Kalousek <109150476+Kvica00@users.noreply.github.com>
2025-01-23 23:41:06 +0100

    uh neco

--- Assets/RBMan.cs
+++ Assets/RBMan.cs
@@ -1,13 +1,26 @@
 using UnityEngine;
 using System.Linq;
+using Unity.Mathematics;
 
 public class RBMan : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    public Transform[] RBs;
+    [SerializeField]
+    private Transform[] RBs;
+    [SerializeField]
+    private ComputeShader Forces;
     void Start()
     {
         RBs = GameObject.FindGameObjectsWithTag("Body").Select(p => p.transform).ToArray();
+    }
+    private void FixedUpdate()
+    {
+        ComputeBuffer bufferVels = new ComputeBuffer(RBs.Length, sizeof(float) * 3);
+        ComputeBuffer bufferPoss = new ComputeBuffer(RBs.Length, sizeof(float) * 3);
+
+        Forces.SetBuffer(0, "vels", bufferVels);
+        Forces.SetBuffer(0, "poss", bufferPoss);
+        Forces.Dispatch(0, RBs.Length/10, 1, 1);
     }
     void updateArray()
     {
